5240c8917d2e201c7ffa9fa7d9e5f437a3ddb4f8
Dhirender Pratap Singh <division.pvt0@gmail.com>
2023-12-07 07:17:32 +0530

    adding handlers in utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//two ways of writting the handler 1) by promise 2) async
+
+const asyncHandler =(requestHandler) => {
+return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
+  }
+}
+
+export { asyncHandler }
+
+//way2
+// higher order function passing
+
+// const asyncHandler =(requestHandler) => async(req, res, next) => {
+//     try{
+
+//     }
+//     catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
